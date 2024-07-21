e3f820a0b30ba1c124482d6c4201ff3953da3f69
Lorenzo Amata <l.amata@reply.it>
2024-07-21 19:23:44 +0200

    first commit initial SpringBoot setup

---
+++ src/main/java/com/digitalflooding/archie_module/ArchieModuleApplication.java
@@ -0,0 +1,13 @@
+package com.digitalflooding.archie_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchieModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchieModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalflooding/archie_module/ArchieModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalflooding.archie_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchieModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
